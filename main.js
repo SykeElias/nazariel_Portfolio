44dae53380b252ab5c1e56931095d8be54797fd3
SykeElias <115668067+SykeElias@users.noreply.github.com>
2025-04-10 20:29:21 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// script.js
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    navbar.classList.toggle('active');
+};
+
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+};
